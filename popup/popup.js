1d7b12e8ac7d2469399bf74e85ade5aedecffae3
Jamil Abou Ltaif <jaboulta@ad.inria.fr>
2025-11-22 13:07:36 +0100

    update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025, SWiPE X / COATI-DIANA
+
 document.addEventListener('DOMContentLoaded', () => {
   chrome.storage.local.get(
     ['videosWatched', 'totalWatchedTime', 'avgPercentWatched', 'videoHistory'],
